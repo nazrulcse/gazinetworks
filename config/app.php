cb840fdadf9ef235b220623fda611b1b06fd04ff
Nazrul <nazrulku07@gmail.com>
2017-12-11 15:17:25 +0600

    add reporting page

--- config/app.php
+++ config/app.php
@@ -237,7 +237,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Entrust'   => Zizaco\Entrust\EntrustFacade::class,
-
+        'Carbon' => 'Carbon\Carbon',
 
     ],
 
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ Route::get('/','WelcomeController@index');
 Auth::routes();
 
 Route::get('/dashboard', 'HomeController@index')->name('dashboard');
+Route::get('/report/income', 'ReportsController@income')->name('income_report');
+Route::get('/report/expense', 'ReportsController@expense')->name('expense_report');
 
 Route::resource('users', 'UsersController');
 
